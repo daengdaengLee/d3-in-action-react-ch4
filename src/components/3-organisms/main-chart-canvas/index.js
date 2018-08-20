a58794654dc66dbfa595e9e27a2688fda08b2b30
kunholee <gunho1020@gmail.com>
2018-08-20 23:06:32 +0900

    ENH: Draw scatter chart

--- src/components/3-organisms/main-chart-canvas/index.js
+++ src/components/3-organisms/main-chart-canvas/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import styled from 'styled-components';
+import ScatterChart from 'components/2-molecules/scatter-chart';
 
 const CanvasContainer = styled.div`
   width: 100%;
@@ -9,7 +10,7 @@ const CanvasContainer = styled.div`
 
 const MainChartCanvas = () => (
   <CanvasContainer>
-    <Route path="/scatter" render={() => 'hi'} />
+    <Route path="/scatter" component={ScatterChart} />
   </CanvasContainer>
 );
 
