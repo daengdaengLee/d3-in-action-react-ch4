4eb8c9b8e1b5360ff77e0e0f94fda5a29e5fda3c
kunholee <gunho1020@gmail.com>
2018-08-20 22:07:51 +0900

    ENH: Main main header

--- src/components/5-pages/main-page/index.js
+++ src/components/5-pages/main-page/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import MainTemplate from 'components/4-templates/main-template';
+import MainHeader from 'components/3-organisms/main-header';
 
 const MainPage = () => (
-  <MainTemplate top={() => {}} left={() => {}} center={() => {}} />
+  <MainTemplate top={() => <MainHeader />} left={() => {}} center={() => {}} />
 );
 
 export default MainPage;
