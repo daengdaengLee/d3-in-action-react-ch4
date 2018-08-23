80760e2488fe00762c9f6108ca7598f3dd857dfb
daengdaenglee <gunho1020@gmail.com>
2018-08-23 13:20:41 +0900

    ENH: Init HistogramChartFive component

---
+++ src/components/2-molecules/histogram-chart-five/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HistogramChartFive = () => <div>hi</div>;
+
+export default HistogramChartFive;
--- src/components/3-organisms/main-chart-canvas/index.js
+++ src/components/3-organisms/main-chart-canvas/index.js
@@ -5,6 +5,7 @@ import ScatterChart from 'components/2-molecules/scatter-chart';
 import BoxplotChart from 'components/2-molecules/boxplot-chart';
 import LineChart from 'components/2-molecules/line-chart';
 import StreamChart from 'components/2-molecules/stream-chart';
+import HistogramChartFive from 'components/2-molecules/histogram-chart-five';
 
 const CanvasContainer = styled.div`
   width: 100%;
@@ -17,6 +18,7 @@ const MainChartCanvas = () => (
     <Route path="/4/boxplot" component={BoxplotChart} />
     <Route path="/4/linechart" component={LineChart} />
     <Route path="/4/streamchart" component={StreamChart} />
+    <Route path="/5/histogram" component={HistogramChartFive} />
   </CanvasContainer>
 );
 
