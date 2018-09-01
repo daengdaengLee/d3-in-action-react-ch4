199894830b8782d6848a00e7aa32229b0677b4df
daengdaenglee <gunho1020@gmail.com>
2018-09-01 12:28:03 +0900

    ENH: Draw stack bar chart

--- src/components/2-molecules/stack-bar-five/index.js
+++ src/components/2-molecules/stack-bar-five/index.js
@@ -13,25 +13,46 @@ class StackBarFive extends Component {
     this.state = {
       data: [],
     };
-    this._colorScale = this._colorScale.bind(this);
-    this._stackArea = this._stackArea.bind(this);
+    this._xAxis = this._xAxis.bind(this);
+    this._yAxis = this._yAxis.bind(this);
   }
 
   render() {
-    const { _stackLayout, _colorScale, _stackArea } = this;
+    const {
+      _stackLayout,
+      _colorScale,
+      _xScale,
+      _yScale,
+      _xAxis,
+      _yAxis,
+    } = this;
     const { data } = this.state;
+    const xScale = _xScale();
+    const yScale = _yScale();
+    const xAxis = _xAxis(xScale);
+    const yAxis = _yAxis(yScale);
     const stackData = data.length === 0 ? [] : _stackLayout(data);
     stackData.sort((a, b) => a.index - b.index);
     return (
       <Svg>
-        {stackData.map(series => (
-          <path
-            key={series.key}
-            className={series.key}
-            fill={_colorScale(series.key)}
-            d={_stackArea(series)}
-          />
-        ))}
+        <g transform="translate(80, 80)">
+          <g className="xAxisG" ref={el => d3.select(el).call(xAxis)} />
+          <g className="yAxisG" ref={el => d3.select(el).call(yAxis)} />
+          {stackData.map(stack => (
+            <g key={stack.key}>
+              {stack.map((obj, i) => (
+                <rect
+                  key={i}
+                  x={xScale(obj.data.day) - 15}
+                  y={yScale(obj[1])}
+                  width="30"
+                  height={Math.abs(yScale(obj[0]) - yScale(obj[1]))}
+                  fill={_colorScale(stack.key)}
+                />
+              ))}
+            </g>
+          ))}
+        </g>
       </Svg>
     );
   }
@@ -47,18 +68,34 @@ class StackBarFive extends Component {
       .then(data => this.setState({ data }));
   }
 
-  _xScale(x) {
+  _xScale() {
     return d3
       .scaleLinear()
       .domain([0, 10])
-      .range([0, 500])(x);
+      .range([0, 500]);
   }
 
-  _yScale(y) {
+  _yScale() {
     return d3
       .scaleLinear()
       .domain([0, 100])
-      .range([500, 0])(y);
+      .range([500, 0]);
+  }
+
+  _xAxis(xScale) {
+    return d3
+      .axisBottom()
+      .scale(xScale)
+      .tickSizeInner(510)
+      .ticks(10);
+  }
+
+  _yAxis(yScale) {
+    return d3
+      .axisRight()
+      .scale(yScale)
+      .tickSizeInner(540)
+      .ticks(8);
   }
 
   _colorScale(movie) {
@@ -68,22 +105,8 @@ class StackBarFive extends Component {
       .range(d3.schemeCategory10)(movie);
   }
 
-  _stackArea(value) {
-    const { _xScale, _yScale } = this;
-    return d3
-      .area()
-      .curve(d3.curveBasis)
-      .x(d => _xScale(d.data.day))
-      .y0(d => _yScale(d[0]))
-      .y1(d => _yScale(d[1]))(value);
-  }
-
   _stackLayout(data) {
-    return d3
-      .stack()
-      .keys(data.columns.filter(col => col !== 'day'))
-      .offset(d3.stackOffsetSilhouette)
-      .order(d3.stackOrderInsideOut)(data);
+    return d3.stack().keys(data.columns.filter(col => col !== 'day'))(data);
   }
 }
 
