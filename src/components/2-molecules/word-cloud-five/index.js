13e01351bd2c909e4b64936219d4385585eefeeb
daengdaenglee <gunho1020@gmail.com>
2018-09-02 13:06:45 +0900

    ENH: Draw word cloud

--- src/components/2-molecules/word-cloud-five/index.js
+++ src/components/2-molecules/word-cloud-five/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { scaleLinear, csvParse } from 'd3';
+import cloud from 'd3-cloud';
 import CSVTable from 'components/2-molecules/csv-table';
 import worddataCSV from 'assets/resources/worddata';
 
@@ -16,12 +18,66 @@ const Svg = styled.svg`
 `;
 
 class WordCloudFive extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: csvParse(worddataCSV),
+      keywords: ['layout', 'zoom', 'circle', 'style', 'append', 'attr'],
+    };
+  }
   render() {
+    const { _cloud, _wordScale, _randomRotate } = this;
+    const wordScale = _wordScale();
+    const randomRotate = _randomRotate();
+    const { data, keywords } = this.state;
+    const cloud = _cloud(data, wordScale, randomRotate);
+    const words = cloud.words();
     return (
       <Container>
-        <Svg>hi</Svg>
+        <Svg>
+          <g className="wordCloudG" transform="translate(300, 300)">
+            {words.map(word => (
+              <text
+                key={word.text}
+                fontSize={`${word.size}px`}
+                opacity="0.75"
+                textAnchor="middle"
+                transform={`translate(${word.x}, ${word.y})rotate(${
+                  word.rotate
+                })`}
+                fill={keywords.includes(word.text) ? 'red' : 'black'}
+              >
+                {word.text}
+              </text>
+            ))}
+          </g>
+        </Svg>
         <CSVTable csv={worddataCSV} />
       </Container>
+    );
+  }
+
+  _wordScale() {
+    return scaleLinear()
+      .domain([0, 75])
+      .range([10, 160]);
+  }
+
+  _randomRotate() {
+    return scaleLinear()
+      .domain([0, 1])
+      .range([-20, 20]);
+  }
+
+  _cloud(data, wordScale, randomRotate) {
+    return (
+      cloud()
+        .size([650, 650])
+        .words(data)
+        // .rotate(() => randomRotate(Math.random()))
+        .rotate(d => (d.text.length > 5 ? 0 : 90))
+        .fontSize(d => wordScale(parseInt(d.frequency, 10)))
+        .start()
     );
   }
 }
